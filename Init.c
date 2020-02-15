6bd56c3bdcd12f615032ac38ca0497aca80ab3ab
Michael Doody <MichaelDoody2017@u.northwestern.edu>
2020-02-14 20:09:09 -0600

    Updated wifi.h, wifi.c, main.c

--- Init.c
+++ Init.c
@@ -4,3 +4,17 @@
  * Created: 2/9/2020 3:49:59 PM
  *  Author: david
  */ 
+
+ #include "compiler.h"
+ #include "board.h"
+ #include "conf_board.h"
+ #include "gpio.h"
+ #include "ioport.h"
+
+ void board_init(void)
+ {
+ 	ioport_init();
+
+ 	ioport_set_pin_dir(PIN_LED, IOPORT_DIR_OUTPUT);
+ 	ioport_set_pin_level(PIN_LED, false);
+ }
